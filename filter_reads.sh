058e5b4954aba2ad1e115790b6a1aebab3916d8f
Jean-Charles Delmas <130393309+JC-Delmas@users.noreply.github.com>
2023-04-12 11:09:31 +0200

    Update filter_reads.sh
    Adding a report on read filtering

--- filter_reads.sh
+++ filter_reads.sh
@@ -5,6 +5,7 @@ input_r1="$1"
 input_r2="$2"
 output_r1="${input_r1%%.*}_filtered.fastq.gz"
 output_r2="${input_r2%%.*}_filtered.fastq.gz"
+report_file="${input_r1%%.*}_rapport.txt"
 
 # Extraire les reads purs GGGGCC pour le fichier R1
 grep -A 3 -B 1 '^GGGGCC$' <(zcat "$input_r1") > "${input_r1%%.*}_GGGGCC.fastq"
@@ -28,3 +29,16 @@ grep -c '^@' "${input_r2%%.*}_3repeats.fastq" > "${input_r2%%.*}_3repeats_count.
 # Concaténer les reads dans un gros fichier R2
 cat "${input_r2%%.*}_GGGGCC.fastq" "${input_r2%%.*}_3repeats.fastq" | gzip > "$output_r2"
 
+# Créer le rapport
+{
+  echo "Compte-rendu de l'extraction"
+  echo "Extraction des reads avec le motif recherché dans des fichiers intermédiaires"
+  echo
+  echo "Fichiers traités : $input_r1 et $input_r2"
+  echo "Motif recherché : GGGGCC"
+  echo
+  echo "Reads dans ${input_r1%%.*}_3repeats.fastq : $(grep -c '^@' "${input_r1%%.*}_3repeats.fastq")"
+  echo "Reads dans ${input_r2%%.*}_3repeats.fastq : $(grep -c '^@' "${input_r2%%.*}_3repeats.fastq")"
+  echo
+  echo "$(date)"
+} > "$report_file"
